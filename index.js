819e6d96647951242d4833eecd7928a33fd6a443
Egor <zangedzu333gig@gmail.com>
2018-10-16 20:46:40 +0300

    Task 1-3

--- index.js
+++ index.js
@@ -7,4 +7,28 @@ const actors = require('./data/actors.json');
 (async () => {
     await db.sequelize.sync({force: true});
 
+    // 1. Валидация полей budget, year и rating фильма
+    await db.films.create({
+        title: 'Баллада о солдате',
+        rating: 8.241,
+        year: 1959,
+        budget: 24423,
+        gross: 35020,
+        poster: 'https://st.kp.yandex.net/images/film_iphone/iphone360_46019.jpg',
+        position: 100
+    });
+
+    // 2. Пакетная вставка 3 фильмов
+    await db.films.bulkCreate(films.slice(0,3));
+
+    // 3. Пакетное обновление поля liked у актеров с 3 фильмами
+
+    await db.actors.update({
+        liked: 999
+    },
+    {
+        where:{
+            films: 3
+        }    
+    });
 })();
